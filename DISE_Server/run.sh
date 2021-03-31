d8550177f158fb9e28c1a5882024a6089cec634f
Garrett Christian <garrettchristian2@gmail.com>
2021-03-31 09:43:33 -0700

    added another echo

--- DISE_Server/run.sh
+++ DISE_Server/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Script to compile and run the dise server
-echo "Starting DISE Server."
+echo "Compiling DISE Server."
 cd src
 qmake && make
 
+echo "Starting DISE Server."
 cd ..
 ./src/DiseServer
